9c739e32e6c20733b5ce538444d722e5576fd87e
Pål Edman <pal.edman@aller.com>
2025-08-21 16:58:28 +0200

    somehow disappeared from npm

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## Unreleased
 
+## [0.2.3] - 2025-08-21
+
+- republish
+
 ## [0.2.2] - 2025-05-21
 
 - structure timestamp as object with UTC seconds from epoch and fractional seconds as nanoseconds as stated in docs
--- eslint.config.js
+++ eslint.config.js
@@ -42,8 +42,16 @@ const rules = {
   'no-undef': 2,
   'no-underscore-dangle': 0,
   'no-unused-expressions': 2,
-  'no-unused-vars': 2,
-  'no-use-before-define': 0,
+  'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+  'no-use-before-define': [
+    'error',
+    {
+      functions: false,
+      classes: false,
+      variables: true,
+      allowNamedExports: false,
+    },
+  ],
   'no-var': 2,
   'no-with': 2,
   'prefer-const': ['error', { destructuring: 'all' }],
